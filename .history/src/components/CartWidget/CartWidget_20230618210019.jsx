4cfb9ccaa332fe574754a3e4334920502bd2ee84
Héctor Coronado <hm.covar@gmail.com>
2023-06-19 00:16:19 -0500

    !

---
+++ .history/src/components/CartWidget/CartWidget_20230618210019.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+const CartWidget = () => {
+  return (
+    <div>
+      <a href="https://www.google.com.mx/?hl=es" style={{ color: "black" }}>
+        <ShoppingCartIcon />
+      </a>
+    </div>
+  );
+};
+
+export default CartWidget;
